095fa5726c6d067836acc420ddc2e819facabaf2
Fidan <fidansaid@gmail.com>
2016-11-27 08:43:31 +0000

    model and controllers

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ActiveRecord::Base
-   has_many :accounts
+   has_one :accounts
   has_one :user_info
 
   # Include default devise modules. Others available are:
-  #  :lockable, :timeoutable and :omniauthable
+  #, :confirmable  :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :confirmable
+         :recoverable, :rememberable, :trackable, :validatable
 end
